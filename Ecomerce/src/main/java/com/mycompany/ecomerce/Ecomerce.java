ec4ec50694d9aac2372b8dcf1bf2b52be6d3c29a
Carvalho-F5 <felipecarvalho060100@gmail.com>
2023-11-21 09:55:58 -0300

    Alteração do nome da classe principal para cliente

---
+++ Ecomerce/src/main/java/com/mycompany/ecomerce/Ecomerce.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ecomerce;
+
+/**
+ *
+ * @author 82317363
+ */
+public class Ecomerce {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
